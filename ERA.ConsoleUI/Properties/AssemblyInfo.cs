ffc9ffee3f063d0348dbaced36cf6f29b8b76a70
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-07 16:35:39 +0100

    Renamed csproj ERA.ConsoleApi -> ERA.ConsoleUI

--- ERA.ConsoleUI/Properties/AssemblyInfo.cs
+++ ERA.ConsoleUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ERA.ConsoleApi")]
+[assembly: AssemblyTitle("ERA.ConsoleUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ERA.ConsoleApi")]
+[assembly: AssemblyProduct("ERA.ConsoleUI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
