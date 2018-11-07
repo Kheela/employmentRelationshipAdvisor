c0a58f0a5bb8e1f9b47deb2744f3d1f61392918c
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-07 16:57:34 +0100

    Separating WebApi from ng5 code - moved WebApi part to new project ERA.WebApi

---
+++ ERA.WebApi/Webapi/Calculation/PermanentContract/SchedulePermanentContractSalaryCalculationController.cs
@@ -0,0 +1,29 @@
+﻿using ERA.Calculation.PermanentContract;
+using System.Web.Http;
+
+namespace ERA.Web.Webapi.Calculation.PermanentContract
+{
+    public class SchedulePermanentContractSalaryCalculationController : ApiController
+    {
+        // GET api/SchedulePermanentContractSalaryCalculation
+        public PermanentContractSalaryCalculationResult Get(decimal salaryBrutto)
+        {
+            var calculator = new PermanentContractSalaryCalculator();
+
+            var context = new PermanentContractSalaryCalculationContext
+            {
+                EmployeeContributionParameters = new PermanentContractEmployeeContributionParameters
+                {
+                    SocialInsuranceContributionParameters = PermanentContractEmployeeExampleValues.SocialInsuranceContributionParameters,
+                    DeductibleParameters = PermanentContractEmployeeExampleValues.DeductibleParameters,
+                    HealthInsuranceContributionParameters = PermanentContractEmployeeExampleValues.HealthInsuranceContributionParameters,
+                    TaxRelief = PermanentContractEmployeeExampleValues.TaxRelief
+                }
+            };
+
+            var calculationResult = calculator.Calculate(salaryBrutto, context);
+
+            return calculationResult;
+        }
+    }
+}
