51511cebb6da4e1dc24f4e3251bdf31d9c5f168a
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-08 17:54:38 +0100

    9_PermanentContract_microservice_should_be_called_PermanentContractSalaryCalculation: Renamed project ERA.PermanentContract.WebApi -> ERA.PermanentContractSalaryCalculation.WebApi

--- ERA.PermanentContract.WebApi/Properties/AssemblyInfo.cs
+++ ERA.PermanentContract.WebApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ERA.PermanentContract.WebApi")]
+[assembly: AssemblyTitle("ERA.PermanentContractSalaryCalculation.WebApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ERA.PermanentContract.WebApi")]
+[assembly: AssemblyProduct("ERA.PermanentContractSalaryCalculation.WebApi")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
