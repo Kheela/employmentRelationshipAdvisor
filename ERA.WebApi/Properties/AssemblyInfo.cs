185eb33b6cf6f5f5429b87a5bdda96ad4d50eb73
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-08 13:17:49 +0100

    1_Prepare_solution_for_microservices: Adjusted project ERA.WebApi: renamed -> ERA.PermanentContract.WebApi

--- ERA.WebApi/Properties/AssemblyInfo.cs
+++ ERA.WebApi/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ERA.WebApi")]
+[assembly: AssemblyTitle("ERA.PermanentContract.WebApi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ERA.WebApi")]
+[assembly: AssemblyProduct("ERA.PermanentContract.WebApi")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
