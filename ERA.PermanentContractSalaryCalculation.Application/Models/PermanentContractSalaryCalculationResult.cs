4b94807b9f316e8aa9e609ea7d0e70504fbf2724
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-09 19:24:23 +0100

    11_Permanent_Contract_-_refactor_calculation_with_BDD_test: Arranged properties in PermanentContractSalaryCalculationResult

--- ERA.PermanentContractSalaryCalculation.Application/Models/PermanentContractSalaryCalculationResult.cs
+++ ERA.PermanentContractSalaryCalculation.Application/Models/PermanentContractSalaryCalculationResult.cs
@@ -3,27 +3,23 @@
     public class PermanentContractSalaryCalculationResult
     {
         public decimal SalaryGross { get; set; }
-
-        public decimal TotalSocialInsurance { get; set; }
+        public decimal SalaryNett { get; set; }
 
         public decimal RetirementInsurance { get; set; }
         public decimal DisabilityPensionInsurance { get; set; }
         public decimal SicknessInsurance { get; set; }
-
-        public decimal TotalDeductibles { get; set; }
-
-        public decimal CopyrightLawsValue { get; set; }
-        public decimal CopyrightLawsDeductibles { get; set; }
-
-        public decimal TaxBase { get; set; }
+        public decimal TotalSocialInsurance { get; set; }
 
         public decimal HealthInsurance { get; set; }
         public decimal HealthInsuranceDeductibles { get; set; }
         public decimal HealthInsurancePaidFromNett { get; set; }
 
+        public decimal CopyrightLawsValue { get; set; }
+        public decimal CopyrightLawsDeductibles { get; set; }
+        public decimal TotalDeductibles { get; set; }
+
+        public decimal TaxBase { get; set; }
         public decimal TaxMonthlyExemption { get; set; }
         public decimal Tax { get; set; }
-
-        public decimal SalaryNett { get; set; }
     }
 }
