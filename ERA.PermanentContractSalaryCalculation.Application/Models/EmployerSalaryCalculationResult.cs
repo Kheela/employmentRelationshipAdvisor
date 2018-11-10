078a44411b8af51c4be14b1a203a89bb6d332b60
Kheela (home) <katarzyna.ciechanska@gmail.com>
2018-11-10 15:43:02 +0100

    13_Permanent_Contract_-_add_calculation_of_employer_costs: Added model for employer costs

---
+++ ERA.PermanentContractSalaryCalculation.Application/Models/EmployerSalaryCalculationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace ERA.PermanentContractSalaryCalculation.Application.Models
+{
+    public class EmployerSalaryCalculationResult
+    {
+        public decimal TotalPaymentCost { get; set; }
+        public decimal SocialInsurance { get; set; }
+        public decimal HealthInsurance { get; set; }
+        public decimal AccidentInsurance { get; set; }
+        // Fundusz Pracy
+        public decimal LabourFund { get; set; }
+        // Fundusz Gwarantowanych Świadczeń Pracowniczych
+        public decimal GuaranteedEmploymentBenefitFund { get; set; }
+    }
+}
